e4b19ca2e2de472a27fc26b333b67df224200c2a
Admin <Admin@DESKTOP-HNKAPBB>
2019-02-11 18:53:16 +0600

    Web Api Consumption from C# Client

---
+++ Web_Api_Consume_CSharp/Web_Api_Consume_CSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Api_Consume_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunClient();
+            Console.ReadLine();
+        }
+
+
+
+        static void RunClient()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8080"); // Base Server Address (Change Your Address)
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                GetById(client, "1").Wait();
+            }
+        }
+
+
+        static async Task GetById(HttpClient client, string id)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("api/UrlGeneration/" + id); // api/Controller/Parameter
+                response.EnsureSuccessStatusCode();
+
+                string url = await response.Content.ReadAsAsync<String>();
+                Console.WriteLine(url);
+
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+
+
+
+    }
+}
